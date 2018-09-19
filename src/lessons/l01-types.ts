8c4583fef0f219deba36ed9b71c281e3b0f8fa90
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2018-09-19 15:37:09 +0200

    lesson01

--- src/lessons/l01-types.ts
+++ src/lessons/l01-types.ts
@@ -2,4 +2,9 @@ import { lessons } from "./commons";
 
 lessons['lesson01'] = function() {
     console.log(`------------------------------------\n- Lesson 01 - types\n------------------------------------`);
+
+    // boolean
+    let done: boolean = false;
+    console.log("boolean", done);
+
 };
