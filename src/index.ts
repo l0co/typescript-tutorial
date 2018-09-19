b8380e44e56ac818455d783b68cca22fd9383f32
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2018-09-19 15:25:07 +0200

    dynamic lesson loader and chooser

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-import lesson01 from "./lessons/l01-types";
+import { lessons } from "./lessons/commons";
+import "./lessons/l01-types";
 
-lesson01();
+if (process.argv.length<=2) {
+
+    console.log("Give lesson number as command line argument (eg: 01))");
+    process.exit(1);
+
+} else {
+
+    let lessonNumber: string = process.argv[2];
+    if (lessonNumber.length<2)
+        lessonNumber = "0" + lessonNumber;
+    lessons[`lesson${lessonNumber}`]();
+
+}
---
+++ src/lessons/commons.ts
@@ -0,0 +1,5 @@
+export interface Lessons {
+    [key: string]: any
+}
+
+export let lessons: Lessons = {};
