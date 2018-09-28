c1fc50c337e3c13f29b1b326ff7eb12b43ed361a
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2018-09-28 13:21:41 +0200

    lesson 00 - ecma6

--- src/lessons/l00-ecma6.ts
+++ src/lessons/l00-ecma6.ts
@@ -128,6 +128,81 @@ lessons['lesson00'] = function() {
     };
     x({p1: 'A', p2: 'B'}); // A B
 
+    // classes - same as typescript (class definition, class inheritance, super class access, static members, getter setter)
+    // no private/protected/readonly members
+
+    // symbol - a way to add properties to existing objects without interferring with their original properties
+    x = {
+        prop1: 'val1',
+        prop2: 'val2'
+    };
+    const myProp = Symbol('myProp'); // this is unique symbol regardless its name
+    const myGlobalProp = Symbol.for('my.global.prop'); // this is global shared symbol accessible from registry by unique name
+    x[myProp] = 'SECRET';
+    console.log('\nObject with symbol:',
+        x, // VISIBLE IN DEBUGGER: { prop1: 'val1', prop2: 'val2', [Symbol(myProp)]: 'SECRET' }
+        x[myProp], // ACCESSIBLE IN CODE: SECRET
+        JSON.stringify(x), // NOT ADDED TO JSON OUTPUT: {"prop1":"val1","prop2":"val2"}
+        Object.keys(x), // NOT VISIBLE IN OBJECT KEYS: [ 'prop1', 'prop2' ]
+        Object.getOwnPropertyNames(x), // NOT VISIBLE IN OBJECT PROPERTIES: [ 'prop1', 'prop2' ]
+        Object.getOwnPropertySymbols(x)); // VISIBLE IN OBJECT SYMBOLS: [ Symbol(myProp) ]
+
+    // iterator: object implementing next() method returning {done: boolean, value: any}
+    // interable: object returning iterator in [Symbol.iterator]() method
+    // in typescript iteration only works with "downlevelIteration" compiler flag turned on
+    console.log('\nIterator');
+    x = {
+        props: [1, 2, 3],
+
+        [Symbol.iterator]() {
+            let i = -1;
+            return {
+                props: this.props,
+                next() {
+                    return i++ < this.props.length-1 ? {done: false, value: this.props[i]} : {done: true};
+                }
+            }
+        }
+    };
+    for (let y of x) {
+        console.log(y);
+    }
+
+    // generator: yield exists immediately with value until next next() execution is called
+    // @ts-ignore
+    function* gen(start) {
+        console.log("Yielding start (1)", start);
+        yield start;
+
+        start += 1;
+        console.log("Yielding start (2)", start);
+        yield start;
+
+        console.log("Thanks for using this generator");
+    }
+    x = gen(2);
+    console.log('\nGenerator', x);
+    console.log(x.next()); // Yielding start (1) 2: { value: 2, done: false }
+    console.log(x.next()); // Yielding start (2) 3: { value: 3, done: false }
+    console.log(x.next()); // Thanks for using this generator: { value: undefined, done: true }
+
+    // iterable using generator (sematically same as the one two examples above)
+    console.log('\nIteratable generator');
+    x = {
+        props: [1, 2, 3],
+
+        // @ts-ignore
+        *[Symbol.iterator]() {
+            let i = 0;
+            while (i < this.props.length) {
+                yield this.props[i++];
+            }
+        }
+    };
+    for (let y of x) {
+        console.log(y);
+    }
+
 };
 
 // import/export examples
