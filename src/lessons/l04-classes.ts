ed85be46efa3ca1b07f91a9841fbbb46fe4074ff
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2018-10-05 13:27:13 +0200

    readibility

--- src/lessons/l04-classes.ts
+++ src/lessons/l04-classes.ts
@@ -1,114 +1,125 @@
 import { lessons } from "../commons";
 
 /**
+ * Classes: https://www.typescriptlang.org/docs/handbook/classes.html
+ *
  * @author Lukasz Frankowski
  */
 lessons['lesson04'] = function() {
     console.log(`------------------------------------\n- Lesson 04 - classes\n------------------------------------`);
 
-    // simple example
-    class Greeter {
-        readonly greeting: string; // this needs to be initialized in constructor
+    console.log('\nclass basics');
+    {
+        // simple class
+        class Greeter {
+            readonly greeting: string; // property: readonly = needs to be initialized in constructor
 
-        constructor(greeting: string) {
-            this.greeting = greeting;
-        }
+            constructor(greeting: string) { // constructor
+                this.greeting = greeting;
+            }
 
-        greet(emot: string = null): Greeter {
-            console.log(this.greeting, emot ? emot : '');
-            return this;
+            greet(emot: string = null): Greeter { // method + default argument value
+                console.log(this.greeting, emot ? emot : '');
+                return this;
+            }
         }
+        let greeter = new Greeter('Hello world!');
+        greeter.greet(":)"); // Hello world! :)
+        console.log(greeter.greeting); // Hello world!: public is default property visibility
+
+        // inheritance
+        class BetterGreeter extends Greeter { // extends - same as in java
+            protected numberOfGreetings: number; // protected visiblity
+
+            constructor(greeting: string) {
+                super(greeting); // calling super construtor - same as in java
+                this.numberOfGreetings = 0;
+            }
+
+            greet(emot: string = ':D'): Greeter { // the default value of argument can be overridden
+                super.greet(emot); // calling super method
+                console.log(`(total number of greetings: ${++this.numberOfGreetings})`);
+                return this;
+            }
+        }
+        let betterGreeter = new BetterGreeter('Hello world!');
+        betterGreeter.greet(); // Hello world! :D ... (total number of greetings: 1)
+        betterGreeter.greet(':)'); // Hello world! :) ... (total number of greetings: 2)
+        // console.log(betterGreeter.numberOfGreetings); // Error:(48, 35) TS2445: Property 'numberOfGreetings' is protected and only accessible within class 'BetterGreeter' and its subclasses.
+
+        // class is still constructor function as in JS
+        console.log(typeof BetterGreeter); // function
+
+        // you can create interface from the class
+        interface SomeInterface extends BetterGreeter {}
     }
-    console.log(new Greeter('Hello world!')
-        .greet()
-        .greeting); // public is the default field visibility
 
-    // inheritance
-    class BetterGreeter extends Greeter {
-        protected numberOfGreetings: number;
+    console.log('\nparameter properties in constructor');
+    {
+        // properties can de declared automatically in constructor
+        class ParamPropertiesExample {
+            // public a: string; // can't declare here if I want to declare in constructor
 
-        constructor(greeting: string) {
-            super(greeting);
-            this.numberOfGreetings = 0;
+            constructor(public a: string, protected b: string, readonly c: string) { // may use modifier or "readonly"
+            }
+
+            getB(): string {
+                return this.b;
+            }
         }
-
-
-        greet(emot: string = ':)'): Greeter { // the default value of argument can be overridden
-            super.greet(emot);
-            console.log(`Total number of greetings: ${++this.numberOfGreetings}`);
-            return this;
-        }
+        let paramPropertiesExample: ParamPropertiesExample = new ParamPropertiesExample('a', 'b', 'c');
+        console.log(paramPropertiesExample.a); // a: a is public
+        console.log(paramPropertiesExample.getB()); // b: b is protected, so we get it using method
+        console.log(paramPropertiesExample.c); // c: c by default is public
     }
-    console.log('');
-    new BetterGreeter('Hello better world!')
-        .greet()
-        .greet();
 
-    // parameter properties
-    class ParamPropertiesExample {
-        // public a: string; // can't declare here if I want to declare in constructor
+    console.log('\ngetters and setters');
+    {
+        class GettersSettersExample {
+            protected _prop: string;
 
-        constructor(public a: string, protected b: string, readonly c: string) { // may use modifier or "readonly"
+            get prop(): string {
+                console.log('getting');
+                return this._prop;
+            }
+
+            set prop(value: string) {
+                console.log('setting');
+                this._prop = value;
+            }
         }
-
-        getB(): string {
-            return this.b;
-        }
+        let gettersSettersExample = new GettersSettersExample();
+        console.log(gettersSettersExample.prop); // getting ... undefined: accessing property is calling getter
+        gettersSettersExample.prop = 'value'; // setting: assigning property is calling setter
+        console.log(gettersSettersExample.prop); // getting ... value
     }
-    console.log('');
-    let paramPropertiesExample: ParamPropertiesExample = new ParamPropertiesExample('a', 'b', 'c');
-    console.log('Property a: ', paramPropertiesExample.a); // a is public
-    console.log('Property b: ', paramPropertiesExample.getB()); // b is protected, so we get it using method
-    console.log('Property c: ', paramPropertiesExample.c); // c by default is public
 
-    // getters and setters
-    class GettersSettersExample {
-        protected _prop: string;
+    console.log('\nstatic properties');
+    {
+        class StaticPropertyExample {
+            static text: string;
 
-        get prop(): string {
-            console.log('returning property value');
-            return this._prop;
+            printText(): void {
+                console.log(StaticPropertyExample.text); // can't refer to "this.text"
+            }
+        }
+        StaticPropertyExample.text = 'Hello static world!';
+        new StaticPropertyExample().printText(); // Hello static world!
+    }
+
+    console.log('\nabstract class');
+    {
+        abstract class AbstractExample {
+            abstract printText(): void;
         }
 
-        set prop(value: string) {
-            console.log('setting property value');
-            this._prop = value;
+        class AbstractExampleSubclass extends AbstractExample {
+            printText(): void {
+                console.log('Hello abstract world!');
+            }
         }
-    }
-    console.log('');
-    let gettersSettersExample = new GettersSettersExample();
-    console.log('Before setter:', gettersSettersExample.prop);
-    gettersSettersExample.prop = 'value';
-    console.log('After setter:', gettersSettersExample.prop);
-
-    // static properties
-    class StaticPropertyExample {
-        static text: string;
-        printText(): void {
-            console.log(StaticPropertyExample.text); // can't refet to this.text
-        }
-    }
-    console.log('');
-    StaticPropertyExample.text = 'Hello static world!';
-    new StaticPropertyExample().printText();
-
-    // abstract
-    abstract class AbstractExample {
-        abstract printText(): void;
-    }
-    class AbstractExampleSubclass extends AbstractExample {
-        printText(): void {
-            console.log('Hello abstract world!');
-        }
-    }
-    console.log('');
-    new AbstractExampleSubclass().printText();
-
-    // class is still constructor function as in JS
-    console.log('typeof class', typeof AbstractExampleSubclass);
-
-    // you can create interface from the class
-    interface SomeInterface extends AbstractExample {
+        
+        new AbstractExampleSubclass().printText(); // Hello abstract world!
     }
 
 };
